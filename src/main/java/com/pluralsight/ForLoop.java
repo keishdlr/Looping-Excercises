9b2fe254737e80d2ee9e9e2c73a504eaa4c5429d
keisha De La Rosa <keishdlr@gmail.com>
2025-10-03 12:35:34 -0400

    did step 2: for loop

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int count = 10; count > 0; count--){
+            System.out.println(count);
+        }
+        System.out.println("Launch!");
+    }
+}
